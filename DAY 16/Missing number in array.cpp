9a00df85b0e187a2406248d78f515122f90519c8
Krishna Kumar <122442441+krishnakumar5399@users.noreply.github.com>
2024-02-19 21:26:31 +0530

    Create Missing number in array.cpp

---
+++ DAY 16/Missing number in array.cpp
@@ -0,0 +1,10 @@
+int missingNumber(vector<int>& array, int n) {
+      
+        int sum =0;
+        for(int i=0;i<n-1;i++){
+             sum+=array[i];
+        }
+       
+       int  ans = n*(n+1)/2;
+        return ans-sum;
+    }
