b893cde27d17471a8815e10b013097e82fc66ab8
Krishna Kumar <122442441+krishnakumar5399@users.noreply.github.com>
2024-03-05 13:38:07 +0530

    Add files via upload

---
+++ DAY 30/2264_Largest3_Same_Digit_Number_in_String.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+    string largestGoodInteger(string num) {
+        string s = "" ;
+        int count = 1 ;
+        for(int i = 0 ;i<num.length()-1;i++){
+            if(num[i]==num[i+1]){
+                count++;
+            }
+            else{
+                count = 1;
+            }
+            if(count == 3){
+                if(s.empty()){
+                    s = num.substr(i-1,3);
+                }
+                else if(s[0]<num[i]){
+                   s = num.substr(i-1,3); 
+                }
+                count = 1 ;
+            }
+        }
+        return s;
+    }
+int main() 
+{
+    string num = "6777133339" ;
+    cout<<largestGoodInteger(num)<<endl;
+     
+     
+  return 0;
+}
