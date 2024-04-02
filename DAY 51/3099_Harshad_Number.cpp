d3af037f09cdfd05969045528b932376443371b4
Krishna Kumar <122442441+krishnakumar5399@users.noreply.github.com>
2024-04-02 22:25:25 +0530

    Add files via upload

---
+++ DAY 51/3099_Harshad_Number.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int num = x ;
+        int rev = 0 ;
+        while(x){
+            int rem = x % 10 ;
+            rev += rem ;
+            x /= 10 ;
+        }
+        if(num % rev==0) return rev;
+        else return -1 ;
+        
+    }
+};
+int main() 
+{
+     
+     
+  return 0;
+}
