4ef2cb8af6d68b542299f136b5b762592a07a698
Krishna Kumar <122442441+krishnakumar5399@users.noreply.github.com>
2024-04-02 22:25:08 +0530

    Add files via upload

---
+++ DAY 50/2022_Convert_1D_Array_Into_2D_Array.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+      int size = original.size() ;
+      vector<vector<int>>arr(m,vector<int>(n)) ;
+      if(size != m*n) return {} ;
+      int k = 0 ;
+      for(int i = 0 ;i<m;i++){
+        for(int j = 0 ;j<n;j++){
+            
+            arr[i][j] = original[k++] ;
+            
+        }
+      }
+      return arr ;  
+    }
+};
+int main() 
+{
+     
+     
+  return 0;
+}
