79edf14a4a72dfbef768e034dcb594c2b08f8673
Krishna Kumar <122442441+krishnakumar5399@users.noreply.github.com>
2024-04-02 22:26:32 +0530

    Add files via upload

---
+++ DAY 55/Largest_Element_inArray.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+        return *(max_element(arr.begin(),arr.end()));
+        
+    }
+};
+int main() 
+{
+     
+     
+  return 0;
+}
