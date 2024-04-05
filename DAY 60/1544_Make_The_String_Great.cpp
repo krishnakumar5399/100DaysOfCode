70f20ce52690d5519940f3a0eded2849f70da4c6
Krishna Kumar <122442441+krishnakumar5399@users.noreply.github.com>
2024-04-05 23:26:04 +0530

    Add files via upload

---
+++ DAY 60/1544_Make_The_String_Great.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string makeGood(string s) {
+        string temp ;
+        for(int i = 0 ;i<s.length();i++){
+            if(temp.length()>0 && abs(temp[temp.length()-1] - s[i]) == 32){
+                temp.pop_back() ; 
+            }
+           
+            else{
+                temp.push_back(s[i]) ;
+            }
+        }
+        return temp ;
+    }
+};
+int main() 
+{
+     
+     
+  return 0;
+}
