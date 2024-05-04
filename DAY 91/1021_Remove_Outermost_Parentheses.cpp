649b3000dae48a3fad27ea9a84dacfc4fd5d08bf
Krishna Kumar <122442441+krishnakumar5399@users.noreply.github.com>
2024-05-05 03:37:55 +0530

    Add files via upload

---
+++ DAY 91/1021_Remove_Outermost_Parentheses.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+       // int n = s.size();
+        string ans="";
+        int count = 0;
+        for(char ch:s)
+        {
+            if(ch=='(' && count==0)
+            {
+                count++;
+            }
+            else if(ch=='(' && count>=1)
+            {
+                ans = ans + ch;
+                count++;
+            }
+            else if(ch==')' && count>1)
+            {
+                ans = ans + ch;
+                count--;
+            }
+            else if(ch==')' && count==1)
+            {
+                count--;
+            }
+        }
+        return ans;
+    }
+};
+int main() 
+{
+     
+     
+  return 0;
+}
